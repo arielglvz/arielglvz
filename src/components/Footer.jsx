03d7e2bd8c1b70b6e663c8b4c059c67e512412b7
arielglvz <arielglvz@gmail.com>
2025-02-21 02:11:28 +0800

    fix mt-12 to mb-8 of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='max-w-4xl mx-auto px-4 py-8 border-t border-border mt-12'>
+    <footer className='max-w-4xl mx-auto px-4 py-8 border-t border-border mt-8'>
       <p className="text-sm text-foreground/70">© 2024 Ariel Galvez. All rights reserved.</p>
     </footer>
   )
